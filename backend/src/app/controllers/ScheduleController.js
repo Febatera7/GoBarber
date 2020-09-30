fa403616052e09a69a1b5e3f61fddc66dbe7b7b5
Felipe Nunes <febatera.soares@gmail.com>
2020-09-30 17:24:32 -0300

    Controllers de agendamento, usuário, upload de arquivos e notificações. Devidas rotas e configurações das mesmas.

---
+++ backend/src/app/controllers/ScheduleController.js
@@ -0,0 +1,51 @@
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+import {
+  startOfDay,
+  parseISO,
+  endOfDay
+} from 'date-fns';
+
+class ScheduleController {
+  async index(req, res) {
+    const {
+      date
+    } = req.query;
+    const parseDate = parseISO(date);
+
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true
+      }
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({
+        error: 'User is not a provider'
+      });
+    };
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)]
+        }
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'name'],
+      }
+    });
+
+    return res.json(appointments)
+  }
+}
+
+export default new ScheduleController
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,10 @@ import authMiddleware from './app/middlewares/auth'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -11,11 +15,21 @@ const routes = new Router();
 
 const upload = multer(multerConfig);
 
-routes.post('/files', upload.single('file'), FileController.store)
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/users', UserController.store);
-routes.put('/users', authMiddleware, UserController.update)
+routes.put('/users', authMiddleware, UserController.update);
 
-routes.post('/sessions', SessionController.store);
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', authMiddleware, AppointmentController.index);
+routes.post('/appointments', authMiddleware, AppointmentController.store);
+
+routes.get('/notifications', authMiddleware, NotificationController.index);
+routes.put('/notifications/:id', authMiddleware, NotificationController.update);
+
+routes.get('/schedule', authMiddleware, ScheduleController.index);
+
+routes.post('/sessions', authMiddleware, SessionController.store);
 
 export default routes;
